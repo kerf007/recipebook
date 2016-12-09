66ec22f68b8fc802b082f4ea179a362aa988654c
kerf007 <kulakov.eugene@gmail.com>
2016-12-09 16:34:36 +0300

    added simple structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// init data
+var recipes = [
+  {id: 1, title: "Pumpkin Pie", ingredients: ["Pumpkin Puree", "Sweetened Condensed Milk", "Eggs", "Pumpkin Pie Spice", "Pie Crust"]}, 
+  {id: 2, title: "Spaghetti", ingredients: ["Noodles", "Tomato Sauce", "(Optional) Meatballs"]}, 
+  {id: 3, title: "Onion Pie", ingredients: ["Onion", "Pie Crust", "Sounds Yummy right?"]}
+]; 
+
+class AddRecipeForm extends Component {
+  render() {
+    const { title, ingredients } = this.props;
+    return (
+        <form>
+          <input type="text" value={title}/>
+          <input type="text" value={ingredients.join()}/>
+        </form>
+      )
+  }
+}
+
+
+class RecipeListItem extends Component {
+  render() {
+    const { title, ingredients } = this.props;
+    return(
+        <div>
+          <h4>{title}</h4>
+          <ul>
+            {ingredients.map((item, index) => <li key={index}>{item}</li>)}      
+          </ul>
+          <button>Edit</button>
+          <button>Close</button>
+        </div>
+      )
+  }
+}
+
+
+class RecipeList extends Component {
+  render() {
+    return (
+       <div>
+          {this.props.recipes.map((item) => <RecipeListItem key={item.id} {...item} />)}
+        </div>
+      )
+  }
+}
+
+class AddRecipe extends Component {
+  render() {
+    return <button className="Add-recipe">Add Recipe</button>
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <RecipeList recipes={recipes} />
+        <AddRecipe />
       </div>
     );
   }
